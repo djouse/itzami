5254707da0be51edcf028a245244e2269aeff7ae
José Dias <jdptdias@outlook.pt>
2025-11-14 18:31:13 +0000

    Added content for contact page

---
+++ app/pages/contact-page.tsx
@@ -0,0 +1,16 @@
+import "../styles/style.css";
+
+export function MainPage() {
+  return (
+    <div>
+      <main className="contact-page">
+        <h1>José Dias</h1>
+        <h3>Porto, Portugal</h3>
+        <p>Email: jdptdias@outlook.pt</p>
+        <p>Phone: +351 91xxxxxxx</p>
+        <p>LinkedIn: linkedin.com/in/josedias</p>
+        <p>GitHub: github.com/djouse</p>
+      </main>
+    </div>
+  );
+}
