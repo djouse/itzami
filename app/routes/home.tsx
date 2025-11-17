b70af46017de63cc52d960f337526f9f4b31ba6f
José Dias <uif78559@automotive-wan.com>
2025-11-17 15:02:21 +0000

    infinite scroll effect

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -18,11 +18,49 @@ export default function Home() {
         </div>
         <div className="introduction-section">
           <h1>portfolio website</h1>
-          <p></p>
           <p>hope you enjoy.</p>
         </div>
-        <div className="separator-section">
+        <div className="separator-section" />
+        <div className="skill-wrapper">
+          <ul className="skill-list">
+            <li>C#</li>
+            <li>.NET 9</li>
+            <li>PostgreSQL</li>
+            <li>ELK</li>
+            <li>Azure DevOps</li>
+            <li>AWS Services</li>
+            <li>JavaScript</li>
+            <li>TypeScript</li>
+            <li>Git</li>
+            <li>ASP.NET</li>
+            <li>C, C++</li>
+            <li>Python</li>
+            <li>OpenCV</li>
+            <li>Doxygen</li>
+            <li>Unix</li>
+            <li>Pytorch, Tensorflow</li>
+
+            {/* Duplicate once for seamless looping */}
+            <li>C#</li>
+            <li>.NET 9</li>
+            <li>PostgreSQL</li>
+            <li>ELK</li>
+            <li>Azure DevOps</li>
+            <li>AWS Services</li>
+            <li>JavaScript</li>
+            <li>TypeScript</li>
+            <li>Git</li>
+            <li>ASP.NET</li>
+            <li>C, C++</li>
+            <li>Python</li>
+            <li>OpenCV</li>
+            <li>Doxygen</li>
+            <li>Unix</li>
+            <li>Pytorch, Tensorflow</li>
+          </ul>
         </div>
+        <div className="separator-section" />
+
         <div className="work-section">
           <h1>Work Experience</h1>
 
