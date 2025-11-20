c1671b53cf2106bf2c1d79656155b80e809afdea
José Dias <jdptdias@outlook.pt>
2025-11-20 17:42:51 +0000

    added dynamic hero component and backgroud effects

---
+++ app/components/hero.tsx
@@ -0,0 +1,18 @@
+import "../styles/style.css";
+import { SkillList } from "./skill-list";
+import { BubbleBackground } from "components/ui/shadcn-io/bubble-background";
+export function Hero() {
+  return (
+    <div className="hero"> 
+        <BubbleBackground interactive={true}>
+            <div className="introduction-section">
+                <h1>portfolio website</h1>
+                <p>hope you enjoy.</p>
+            </div>
+            <div className="separator-section" />
+                <SkillList />
+            <div className="separator-section" />
+        </BubbleBackground>
+    </div>
+  );
+}
---
+++ app/components/skill-list.tsx
@@ -0,0 +1,44 @@
+import "../styles/style.css";
+
+export function SkillList() {
+  return (
+    <div className="skill-wrapper">
+        <ul className="skill-list">
+            <li>C#</li>
+            <li>.NET 9</li>
+            <li>PostgreSQL</li>
+            <li>ELK</li>
+            <li>Azure DevOps</li>
+            <li>AWS Services</li>
+            <li>JavaScript</li>
+            <li>TypeScript</li>
+            <li>Git</li>
+            <li>ASP.NET</li>
+            <li>C, C++</li>
+            <li>Python</li>
+            <li>OpenCV</li>
+            <li>Doxygen</li>
+            <li>Unix</li>
+            <li>Pytorch, Tensorflow</li>
+
+            {/* Duplicate once for seamless looping */}
+            <li>C#</li>
+            <li>.NET 9</li>
+            <li>PostgreSQL</li>
+            <li>ELK</li>
+            <li>Azure DevOps</li>
+            <li>AWS Services</li>
+            <li>JavaScript</li>
+            <li>TypeScript</li>
+            <li>Git</li>
+            <li>ASP.NET</li>
+            <li>C, C++</li>
+            <li>Python</li>
+            <li>OpenCV</li>
+            <li>Doxygen</li>
+            <li>Unix</li>
+            <li>Pytorch, Tensorflow</li>
+        </ul>
+    </div>
+  );
+}
