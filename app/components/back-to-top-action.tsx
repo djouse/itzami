367cc86a9e31124086cf1b95f9249bccbcea9bbd
José Dias <jdptdias@outlook.pt>
2025-11-22 17:53:08 +0000

    home route segmented into components

---
+++ app/components/back-to-top-action.tsx
@@ -0,0 +1,9 @@
+import upActivity from "../assets/up-onClick.svg";
+
+export function BackToTop() {
+  return (
+    <div className="up-onClick">
+        <img src={upActivity} alt="Up on Click Activity" className="up-icon" />
+    </div>
+  );
+}   
